f32bfdcb482cfe50053ee3622ee1b2ca433674f1
Jaloliddin Lapasov <jaloliddin630@gmail.com>
2025-03-29 20:56:17 +0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,28 @@
+// import { Component } from '@angular/core';
+// import { RouterOutlet } from '@angular/router';
+
+// @Component({
+//   selector: 'app-root',
+//   imports: [RouterOutlet],
+//   templateUrl: './app.component.html',
+//   styleUrl: './app.component.scss'
+// })
+// export class AppComponent {
+//   title = 'my-first-angular-app';
+// }
+
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  showText = false;
+
+  toggleText() {
+    this.showText = !this.showText;
+  }
+}
